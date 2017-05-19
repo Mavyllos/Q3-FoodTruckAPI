1cbd527948976f0d7090913866918e95c226bc8b
Summer Barclay <evil_lyns@yahoo.com>
2017-05-19 14:49:15 -0700

    Setup files for CouchDB connection and database design

--- FoodTruckAPI/Sources/FoodTruckAPI/FoodTruck.swift
+++ FoodTruckAPI/Sources/FoodTruckAPI/FoodTruck.swift
@@ -68,7 +68,87 @@ public class FoodTruck: FoodTruckAPI {
         self.init(database: databaseName, host: host, port: port, username: username, password: password)
         }
 
+    
+    // look for db, if exists log ok, if not lof error and try to create db. if db returned from create, log ok, if not log error.
     private func setupDb() {
-
+        let couchClient = CouchDBClient(connectionProperties: self.connectionProps)
+        couchClient.dbExists(dbName) { (exists, error) in
+            if (exists) {
+                Log.info("DB exists")
+            } else {
+                Log.error("DB does not exist \(String(describing: error))")
+                couchClient.createDB(self.dbName, callback:  { (db, Error) in
+                    if (db != nil) {
+                        Log.info("DB created!")
+                            self.setupDbDesign(db: db!)
+                    } else {
+                        Log.error("Unable to create DB \(self.dbName): Error \(String(describing: error))")
+                    }
+                })
+            }
+        }
+    }
+    
+    private func setupDbDesign(db: Database) {
+        let design: [String: Any] = [
+            "_id": "_design/foodtruckdesign",
+            "views": [
+                "all_documents": [
+                    "map": "function(doc) { emit(doc._id, [doc._id, doc._rev]); }"
+                ],
+            "all_trucks": [
+                "map": "function(doc) { if (doc.type == 'foodtruck') { emit(doc._id, [doc._id, doc.name, doc.foodtype, doc.avgcost, doc.latitude, doc.longitude]); }}"
+                ],
+            "total_trucks": [
+                "map": "function(doc) { if (doc.type == 'foodtruck') { emit(doc._id, 1);}}",
+                "reduce": "_count"
+                ]
+            ]
+        ]
+        
+        db.createDesign(self.designName, document: JSON(design)) { (json, error) in
+            if error != nil {
+                Log.error("Failed to create design: \(String(describing: error))")
+            } else {
+                Log.info("Design created: \(String(describing: json))")
+            }
+        }
+    }
+    
+    public func getAllTrucks(completion: @escaping ([FoodTruckItem]?, Error?) -> Void) {
+        let couchClient = CouchDBClient(connectionProperties: connectionProps)
+        let database = couchClient.database(dbName)
+        
+        database.queryByView("all_trucks", ofDesign: designName, usingParameters: [.descending(true), .includeDocs(true)]) { doc, err in
+            if let doc = doc, err == nil {
+                do {
+                    let trucks = try self.parseTrucks(doc)
+                    completion(trucks, nil)
+                } catch {
+                    completion(nil, err)
+                }
+            } else {
+                completion(nil, err)
+            }
+        }
+    }
+    
+    func parseTrucks(_ document: JSON) throws -> [FoodTruckItem] {
+        guard let rows = document["rows"].array else {
+            throw APICollectionError.ParseError
+        }
+        let trucks: [FoodTruckItem] = rows.flatMap {
+            let doc = $0["value"]
+            guard let id = doc[0].string,
+                let name = doc[1].string,
+                let foodType = doc[2].string,
+                let avgCost = doc[3].float,
+                let latitude = doc[4].float,
+                let longitude = doc[5].float else {
+                    return nil
+            }
+            return FoodTruckItem(docId: id, name: name, foodType: foodType, avgCost: avgCost, latitude: latitude, longitude: longitude)
+        }
+        return trucks
     }
 }
--- FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckAPI.swift
+++ FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckAPI.swift
@@ -1,2 +1,6 @@
 public protocol FoodTruckAPI {
+    
+    // MARK: Trucks
+    // Get all Food Trucks
+    func getAlllTrucks(completion: @escaping ([FoodTruckItem]?, Error?) -> Void)
 }
