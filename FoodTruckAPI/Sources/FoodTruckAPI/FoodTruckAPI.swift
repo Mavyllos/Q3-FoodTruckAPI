1a691e2984bb550c622713a6ca61bc2562a56def
Summer Barclay <evil_lyns@yahoo.com>
2017-05-19 15:15:09 -0700

    Completed GetALL route

--- FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckAPI.swift
+++ FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckAPI.swift
@@ -2,5 +2,5 @@ public protocol FoodTruckAPI {
     
     // MARK: Trucks
     // Get all Food Trucks
-    func getAlllTrucks(completion: @escaping ([FoodTruckItem]?, Error?) -> Void)
+    func getAllTrucks(completion: @escaping ([FoodTruckItem]?, Error?) -> Void)
 }
--- FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckController.swift
+++ FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckController.swift
@@ -23,5 +23,29 @@ public final class FoodTruckController {
     
     public func routeSetup() {
         router.all("/*", middleware: BodyParser())
+        
+        //Food Truck Handling
+        router.get(trucksPath, handler: getTrucks)
+    }
+    
+    private func getTrucks(request: RouterRequest, response: RouterResponse, next: () -> Void) {
+        trucks.getAllTrucks { (trucks, err) in
+            do {
+                guard err == nil else {
+                    try response.status(.badRequest).end()
+                    Log.error(err.debugDescription)
+                    return
+                }
+                guard let trucks = trucks else {
+                    try response.status(.internalServerError).end()
+                    Log.error("Failed to get trucks")
+                    return
+                }
+                let json = JSON(trucks.toDict())
+                try response.status(.OK).send(json: json).end()
+            } catch {
+                Log.error("Communications error")
+            }
+        }
     }
 }
